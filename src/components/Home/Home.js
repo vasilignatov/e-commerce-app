904731ee82d3feafbe7eaa382ff4277973d04b04
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-29 23:14:51 +0300

    Remove images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import SlidesShop from './SlidesShop.js';
 import CategoriesShop from './CategoriesShop.js';
 import FeaturedProducts from './FeaturedProducts.js';
 import LatestBlog from './LatestBlog.js';
+import Auth from '../Auth/Auth.js';
 
 const Home = () => {
    return (
       <>
+         {/* <Auth></Auth> */}
          {/* <SlidesShop /> */}
          <CategoriesShop />
          <FeaturedProducts />
