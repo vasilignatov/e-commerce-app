85a0b2302c374d28cbeffb22ca458ed2f06f4457
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-31 22:54:55 +0300

    Fix bug with names

--- src/components/Common/AllTitleBox.js
+++ src/components/Common/AllTitleBox.js
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom';
 
 const AllTitleBox = ({ title }) => {
     return (
-        <div class="all-title-box">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
+        <div className="all-title-box">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
 
                         <h2>{title}</h2>
 
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item">
+                        <ul className="breadcrumb">
+                            <li className="breadcrumb-item">
                                 <Link to="/">Home</Link>
                             </li>
                             
-                            <li class="breadcrumb-item active">{title}</li>
+                            <li className="breadcrumb-item active">{title}</li>
                         </ul>
                     </div>
                 </div>
