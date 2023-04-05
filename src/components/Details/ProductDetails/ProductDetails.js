7e90456169e08b640183cca8a151404425140fd3
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-05 23:15:51 +0300

    Add functionality for size dropdown

--- src/components/Details/ProductDetails/ProductDetails.js
+++ src/components/Details/ProductDetails/ProductDetails.js
@@ -1,18 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import ProductContext from '../../../contexts/ProductContext.js';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import './ProductDetails.css';
 
 const ProductDetails = () => {
 
-    const { product } = useContext(ProductContext);
+    const { product, isLoading } = useContext(ProductContext);
+    const [currentSize, setCurrentSize] = useState('Choose your size');
     console.log(product);
+
+
+
+    function onSizeClickHandler(e) {
+        e.preventDefault();
+        if (e.target.tagName == 'A' && e.target.classList.contains('active')) {
+            setCurrentSize(e.target.textContent)
+        }
+    }
+
+
+
     return (
         <div className="col-xl-7 col-lg-7 col-md-6">
             <div className="single-product-details">
+                <p className="product-categories">
+                    <a className="category">{product.category}</a>
+                    <span> / </span>
+                    <a className="subcategory">{product.subCategory}</a>
+                </p>
                 <h2>{product.title}</h2>
                 <h5>${product.price}</h5>
                 <p className="available-stock">
                     <span>
-                        More than 20 available / <a href="#">8 sold </a>
+                        More than 20 available / <a href="#">{product.soldCount} sold </a>
                     </span>
                 </p>
                 <p></p>
@@ -24,30 +45,33 @@ const ProductDetails = () => {
                     <li>
                         <div className="form-group size-st">
                             <label className="size-label">Size</label>
-                            <select
-                                id="basic"
-                                className="selectpicker show-tick form-control"
-                            >
 
+                            <DropdownButton
+                                id="dropdown-button-dark-example2"
+                                variant="secondary"
+                                menuVariant="dark"
+                                title="Dropdown button"
+                                className="mt-2"
+                                onClick={onSizeClickHandler}
+                                title={currentSize}
+                            >
                                 {
-                                    product.sizesAvailable.map(s => {
-                                        <option value={s}>{s}</option>
-                                    })
+                                    !isLoading
+                                        ? Object.entries(product.sizesAvailable)
+                                            .map(([k, v]) => {
+                                                return <Dropdown.Item eventKey={k} active={v}>{k}</Dropdown.Item>
+                                            })
+                                        : ''
                                 }
-                                
-                            </select>
+
+                            </DropdownButton>
+
                         </div>
                     </li>
                     <li>
                         <div className="form-group quantity-box">
                             <label className="control-label">Quantity</label>
-                            <input
-                                className="form-control"
-                                defaultValue={0}
-                                min={0}
-                                max={20}
-                                type="number"
-                            />
+                            <input className="form-control" defaultValue={0} min={0} max={20} type="number" />
                         </div>
                     </li>
                 </ul>
@@ -63,13 +87,16 @@ const ProductDetails = () => {
                 </div>
                 <div className="add-to-btn">
                     <div className="add-comp">
+
                         <a className="btn hvr-hover" href="#">
                             <i className="fas fa-heart" /> Add to wishlist
-                    </a>
-                        <a className="btn hvr-hover" href="#">
+                        </a>
+
+                        {/*<a className="btn hvr-hover" href="#">
                             <i className="fas fa-sync-alt" /> Add to Compare
-                    </a>
+                        </a> */}
                     </div>
+
                     <div className="share-bar">
                         <a className="btn hvr-hover" href="#">
                             <i className="fab fa-facebook" aria-hidden="true" />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import SlidesShop from './SlidesShop.js';
 import CategoriesShop from './CategoriesShop.js';
-import FeaturedProducts from './FeaturedProducts.js';
+import FeaturedProducts from '../Common/FeaturedProducts.js';
 import LatestBlog from './LatestBlog.js';
 import Auth from '../Auth/Auth.js';
 
