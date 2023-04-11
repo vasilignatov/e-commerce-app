5808ec5caeb30958dcff826718e2348b6c655edd
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-11 23:03:32 +0300

    Added Auth service

---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+import { endpoints } from '../endpoints.js';
+import { request, getOptions } from './utils.js';
+
+export async function login(email, password) {
+    return await request(endpoints.login, getOptions('POST', { email, password }));
+}
+
+export async function register(username, password, email) {
+    return await request(endpoints.register, getOptions('POST', { email, username, password }));
+}
+
+export async function logout() {
+    return await request(endpoints.logout, getOptions('POST'));
+}
+
+
+
