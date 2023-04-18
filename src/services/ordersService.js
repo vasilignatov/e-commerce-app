4c0ec28b80a6a75caf11bd25d6787bb7273822bb
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-19 00:36:15 +0300

    Fixed bug

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -2,7 +2,7 @@ import { endpoints } from '../endpoints.js';
 import { request, getOptions } from './utils.js';
 
 export async function createOrder(data, userId) {
-    return await request(endpoints.order, getOptions('POST', { data, userId }));
+    return await request(endpoints.postOrder, getOptions('POST', { data, userId }));
 }
 
 export async function getOrdersById(id) {
