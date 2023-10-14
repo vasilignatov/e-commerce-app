24253027f3701a1e68d106372625e0cf85ccb4b4
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-14 16:11:33 +0300

    Edited endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,14 +1,19 @@
-const host = 'http://localhost:3030'
+const host = 'http://localhost:3000'
 
 export const endpoints = {
+    // auth routes
+    login: `${host}/auth/login`,
+    register: `${host}/auth/register`,
+    logout: `${host}/auth/logout`,
+    // products routes
     getProducts: `${host}/products/`,
     getProductById: `${host}/products/`,
     createProduct: `${host}/products/`,
-    login: `${host}/users/login`,
-    register: `${host}/users/register`,
-    logout: `${host}/users/logout`,
+    getLastAdded: `${host}/products/last-added`,
+    // orders routes
     postOrder: `${host}/orders/create`,
     getOrders: `${host}/orders`,
+    // whishlist routes
     getWhishlist: `${host}/whishlist`,
     postWhishlist: `${host}/whishlist`,
     deleteWhishlist: `${host}/whishlist`,
