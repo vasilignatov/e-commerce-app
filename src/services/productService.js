58169419c5dd5ef32a720ae1989866b7e403f4a7
Vasil <vasilignatow@gmail.com>
2023-10-15 23:02:48 +0300

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,6 +2,10 @@ import { endpoints } from '../endpoints.js';
 import { request, getOptions } from './utils.js';
 
 export async function getLastAdded() {
-    return await request(endpoints.login, getOptions('GET'));
+    return await request(endpoints.getLastAdded, getOptions('GET'));
+}
+
+export async function getProductCategories() {
+    return await request(endpoints.getProductsCategories, getOptions('GET'));
 }
 
