a0c487601b6734ba481df91971db5ac122e1996a
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-16 23:23:35 +0300

    Update ProductsPage.js

--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -1,7 +1,7 @@
 import SideBar from './SideBar.js';
 import Catalog from './Catalog/Catalog.js';
 import AllTitleBox from '../Common/AllTitleBox';
-
+import { Outlet } from 'react-router-dom';
 const ProductsPage = () => {
 
     return (
