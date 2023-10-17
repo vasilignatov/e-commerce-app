0f5cfdc4d9045c2f3dfdcab8b5c940f2af297178
Vasil <vasilignatow@gmail.com>
2023-10-17 11:29:21 +0300

    Update whishlistService.js

--- src/services/whishlistService.js
+++ src/services/whishlistService.js
@@ -1,6 +1,12 @@
 import { endpoints } from '../endpoints.js';
 import { request, getOptions } from './utils.js';
 
-export async function addProductToWhishlist() {
-    return await request(endpoints.postWhishlist, getOptions('POST', {}));
+export async function getWhishlist() {
+    return await request(endpoints.getWhishlist, getOptions('GET'));
+}
+export async function addProductToWhishlist(productId) {
+    return await request(endpoints.addProductToWhishlist, getOptions('POST', productId));
+}
+export async function removeFromWhishlist(productId) {
+    return await request(endpoints.removeFromWhishlist, getOptions('DELETE', productId));
 }
