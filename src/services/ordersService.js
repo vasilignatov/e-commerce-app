2c510b943668afdd17b40bd06db28b6fa1059984
Vasil <vasilignatow@gmail.com>
2023-10-17 11:29:51 +0300

    Edited order service

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -1,8 +1,8 @@
 import { endpoints } from '../endpoints.js';
 import { request, getOptions } from './utils.js';
 
-export async function createOrder(data, userId) {
-    return await request(endpoints.postOrder, getOptions('POST', { data, userId }));
+export async function createOrder(data) {
+    return await request(endpoints.postOrder, getOptions('POST', data));
 }
 
 export async function getOrdersById(id) {
